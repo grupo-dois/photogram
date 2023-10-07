8c37500968021be9748de66ccca634a38c51d0dd
Henrique Kalife <henrique.kalife@terceiros.zenvia.com>
2023-10-07 14:46:40 -0300

    chore: change titles for initial development

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -7,13 +7,13 @@ const Home: React.FC = () => {
     <IonPage>
       <IonHeader>
         <IonToolbar>
-          <IonTitle>Blank</IonTitle>
+          <IonTitle>Photogram</IonTitle>
         </IonToolbar>
       </IonHeader>
       <IonContent fullscreen>
         <IonHeader collapse="condense">
           <IonToolbar>
-            <IonTitle size="large">Blank</IonTitle>
+            <IonTitle size="large">Photogram</IonTitle>
           </IonToolbar>
         </IonHeader>
         <ExploreContainer />
